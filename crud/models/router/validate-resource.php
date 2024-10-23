18c0ea6d1cb77c3428148ca246d1ed8df5150918
Hybrid Web Dev <justin_a_lindsay@hotmail.com>
2024-10-23 00:33:55 -0600

    working on router

--- crud/models/router/validate-resource.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace pockets_router\crud\models\router;
-
-trait validate_resource {
-    function validateResource() : bool {
-        return true;
-    }
-}
--- plugin/module.php
+++ plugin/module.php
@@ -15,32 +15,6 @@ class module extends \pockets_forms\base {
         parent::__construct();
 
         api\module::init();
-        \pockets_router\crud\models\router\model::register();
-
-        add_filter( 'template_include', [ $this, 'router' ], 100 );
-
-    }
-
-    function router( $template ){
-
-        if( !$template) {
-            return;
-        }
-
-        get_header();
-            
-            $route = \pockets::crud('node-tree/router')::init(
-                home_url( add_query_arg( null, null ) )
-            )->read( [ 'render', 'source' ] );
-
-            echo sprintf(
-                "<pockets-router class='p-2 bg-danger' :source='%s'></pockets-router>", 
-                $route['source'],
-            );
-
-        get_footer();
-
-        exit;   
 
     }
     
